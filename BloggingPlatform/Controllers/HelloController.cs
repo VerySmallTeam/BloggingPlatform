1e3eb65bf3b25c6276b0061e5c14c669f1cede39
Krystian Ostrowski <krystian.ostrowski@outlook.com>
2020-02-04 17:07:26 +0100

    Initial commit

---
+++ BloggingPlatform/Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloggingPlatform.Controllers
+{
+    [ApiController]
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private static readonly string[] values = new[]
+        {
+            "Hello", "from", "the", "first", "controller", "yay"
+        };
+
+        [HttpGet]
+        public string[] Hello()
+        {
+            return values;
+        }
+    }
+}
