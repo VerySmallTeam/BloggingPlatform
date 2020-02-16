49877a1f821c2a65da9d6f139e6370fa5d8a2058
Krystian Ostrowski <krystian.ostrowski@outlook.com>
2020-02-16 03:49:52 +0100

    New migration added

---
+++ BloggingPlatform/Migrations/20200216024737_AddBasicBlogStructure.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BloggingPlatform.Migrations
+{
+    public partial class AddBasicBlogStructure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
